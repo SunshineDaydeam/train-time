63ecb933f04a1d4cb0eb991a082d12d90bceae4c
Sunshine Daydream <alexbruner@live.com>
2018-05-18 23:59:58 -0500

    labelled stuff

--- assets/java/java.js
+++ assets/java/java.js
@@ -15,52 +15,72 @@ var trainName = "";
 var trainDestination = "";
 var trainFirst = "";
 var trainFrequency = "";
+var trainDisplayArray=[];                                           
 
+//Submit Button Click Handler
+$("#submitBtn").on("click", function(event){
 
-//Submit Button - push info to firebase
+    if ($("#nameInput").val().trim() != "" &&                   //if name input isn't blank &
+        $("#destinationInput").val().trim() != "" &&            //if destination input isn't blank &
+        $("#frequencyInput").val() <=1440 &&                    //if frequency is less than 1 day &
+        isNaN($("#frequencyInput").val()) != true &&            //if frequency is a number &
+        $("#firstTimeInput").val() != ""                        //if first time input isn't blank
+    ){
+        trainName = $("#nameInput").val().trim();               //assign name variable
+        trainDestination = $("#destinationInput").val().trim(); //assign destination variable
+        trainFirst = $("#firstTimeInput").val().trim();         //assign first time variable
+        trainFrequency = $("#frequencyInput").val().trim();     //assign frequency variable
 
+        $("#nameInput").val("");                                //clear input boxes
+    $("#destinationInput").val("");                             //clear input boxes
+        $("#firstTimeInput").val("");                           //clear input boxes
+        $("#frequencyInput").val("");                           //clear input boxes
 
-    $("#submitBtn").on("click", function(event){
-        if ($("#nameInput").val().trim() != "" && $("#destinationInput").val().trim() != "" && $("#frequencyInput").val() <=1440) {
-        trainName = $("#nameInput").val().trim();
-        trainDestination = $("#destinationInput").val().trim();
-        trainFirst = $("#firstTimeInput").val().trim();
-        trainFrequency = $("#frequencyInput").val().trim();
-
-        $("#nameInput").val("");
-        $("#destinationInput").val("");
-        $("#firstTimeInput").val("");
-        $("#frequencyInput").val("");
-
-        var newTrain = {
-            trainName: trainName,
-            trainDestination: trainDestination,
-            trainFirst:trainFirst,
-            trainFrequency: trainFrequency
+        var newTrain = {                                        //Build an object to contain all information
+            trainName: trainName,                               //set trainName to Train Name
+            trainDestination: trainDestination,                 //set trainDestination to Train Destination
+            trainFirst:trainFirst,                              //set trainFirst to Train First
+            trainFrequency: trainFrequency                      //set trainFrequency to train frequency
         };
 
-        database.ref().push(newTrain);
+    database.ref().push(newTrain);                              //Push that object to firebase
     };
 });
 
 
-//Update Train Displays
+//When a child is added (Or when page is loaded)
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-    //Create Variables of names
-    var trainInfoName = (childSnapshot.val().trainName);
-    var trainInfoDest = (childSnapshot.val().trainDestination);
-    var trainInfoStart = (childSnapshot.val().trainFirst);
-    var trainInfoFreq = (childSnapshot.val().trainFrequency);        
+                               
+
+    //Define Variables from Snapshot
+    var trainInfoName = (childSnapshot.val().trainName);        //create name variable from snapshot
+    var trainInfoDest = (childSnapshot.val().trainDestination); //create destination variable from snapshot
+    var trainInfoStart = (childSnapshot.val().trainFirst);      //create start time variable from snapshot
+    var trainInfoFreq = (childSnapshot.val().trainFrequency);   //create frequency variable from snapshot
+    
+    
     //Calculate Times
-    var firstTimeConverted = moment(trainInfoStart, "hh:mm");
-    var currentTime = moment();
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    var displayNextTrainMinutes=(trainInfoFreq-(diffTime%trainInfoFreq));
-    var displayNextTrainTime=moment().add(displayNextTrainMinutes, 'minutes');
+    var firstTimeConverted = moment(trainInfoStart, "hh:mm");                   //convert start time to hours and minutes              
+    var currentTime = moment();                                                 //define current time
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");        //calculate number of minutes between first time and current time
+    var displayNextTrainMinutes=(trainInfoFreq-(diffTime%trainInfoFreq));       //calculate number of minutes til next train
+    var displayNextTrainTime=moment().add(displayNextTrainMinutes, 'minutes');  //calculate the next train time
+
+    // //push info from trains to an object
+    // var trainInfoNameDisplay = {
+    //     "name": trainInfoName, 
+    //     "destination": trainInfoDest, 
+    //     "firstTrain": trainInfoStart, 
+    //     "trainMinutes": trainInfoFreq};
+    // //push those objects to trainDisplayArray
+    // trainDisplayArray.push(trainInfoNameDisplay);
+
+    
+    //Display to Screen//
 
     //tablerow
     var tr = $("<tr>");
-        tr.addClass("border-bottom");
+        tr.addClass("border-bottom remove");
     //train name
     var td1 = $("<td>");
         td1.text(trainInfoName);
@@ -74,20 +94,18 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
         td3.text(trainInfoFreq + " minutes");
     //Next Arrival
     var td4 = $("<td>");
-        td4.addClass("text-muted");
-        td4.attr("id", trainInfoName.replace(/\s/g, ''));
+        td4.addClass("text-muted nextTrain");
+        td4.attr("start", trainInfoStart);
+        td4.attr("freq", trainInfoFreq);
         td4.text(moment(displayNextTrainTime).format('hh:mm'));
-        console.log(displayNextTrainMinutes);
     //Minutes til next train
     var td5 = $("<td>");
-        td5.addClass("text-muted");
+        td5.addClass("text-muted minutes");
+        td4.attr("start", trainInfoStart);
+        td4.attr("freq", trainInfoFreq);
         td5.text(displayNextTrainMinutes);
 
         //display to html
-        tr.append(td1, td2, td3, td4, td5);
-        $("#trainDisplay").append(tr);
-
-
-    console.log(childSnapshot.val().trainDestination);
-
+        tr.append(td1, td2, td3, td4, td5);                     //append all to tr div
+        $("#trainDisplay").append(tr);                          //Display on Screen
 });
